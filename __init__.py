3616b79ea42932a8e43a7c7c38f58eb2c76fc959
Seiya KUBO <seiyakubo@gmail.com>
2014-07-26 20:26:52 +0900

    edit task
    - remove temp file

--- __init__.py
+++ __init__.py
@@ -7,6 +7,7 @@ def nginx_put_repofile():
     repofile = path.dirname(path.abspath(__file__)) + '/repofile/nginx.centos.repo'
     put(repofile, '/tmp')
     sudo('install -v -o root -g root -m 0644 /tmp/nginx.centos.repo /etc/yum.repos.d/nginx.repo')
+    sudo('rm -vf                             /tmp/nginx.centos.repo')
 
 def nginx_install():
     sudo('yum -y install nginx')
